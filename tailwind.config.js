9d036768887ed5442f68fe89eeafad004f7e741b
Sayan Saha <sayansaha1510@gmail.com>
2025-01-10 16:59:36 +0530

    3- Setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{vue,js,ts,jsx,tsx}', // Adjust the path based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
